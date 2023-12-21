08d12c417f003c58b1fa100b7592dc155094bd59
RAHANAZHABEEB <140069447+RAHANAZHABEEB@users.noreply.github.com>
2023-12-21 04:37:22 -0800

    Add files via upload

---
+++ pro/app1/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Dress
+
+admin.site.register(Dress)
+# Register your models here.
---
+++ pro/app1/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.db import models
+class Dress(models.Model):
+    name=models.CharField(max_length=250)
+    price=models.IntegerField()
+    brand=models.CharField(max_length=250)
+    img=models.ImageField(upload_to='gallery')
+
+    def _str_(self):
+        return self.name
+
+# Create your models here.
